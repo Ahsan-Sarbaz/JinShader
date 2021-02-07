17388015c35cfbdae6c3b105820d1b031ba3bd4b
Ahsan-Sarbaz <geeksdotpk@gmail.com>
2021-02-07 19:49:54 +0500

    first working version

--- JinShader/main.cpp
+++ JinShader/main.cpp
@@ -8,6 +8,26 @@
 #define WINDOW_WIDTH 1920
 #define WINDOW_HEIGHT 1080
 
+static bool want_exit = false;
+static bool want_save = false;
+static bool want_update = false;
+static bool compile_success = false;
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		want_exit = true;
+	}
+
+	if (key == GLFW_KEY_S && mods == GLFW_MOD_CONTROL && action == GLFW_PRESS)
+	{
+		want_update = true;
+		want_save = true;
+	}
+}
+
 int main()
 {
 	if (!glfwInit())
@@ -19,6 +39,7 @@ int main()
 	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "JinShader", 0, 0);
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(1);
+	glfwSetKeyCallback(window, key_callback);
 	glewExperimental = true;
 	if (glewInit() != GLEW_OK)
 	{
@@ -35,22 +56,65 @@ int main()
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	const char* glsl_version = "#version 150";
 	ImGui_ImplOpenGL3_Init(glsl_version);
+	
+	unsigned int program = 0, shader = 0;
+	int codeBufferSize = 1024 * 1024 * 4;
+	char* codeBuffer = (char*)calloc(1, codeBufferSize);
 
-	unsigned int program = glCreateProgram();
-	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	//glShaderSource();
+	const char* vertexShaderSource = 
+		"#version 460\n"
+		"layout(location = 0) in vec2 in_position;\n"
+		//"out vec2 fragCoord;\n"
+		"void main()\n"
+		"{\n"
+			"gl_Position = vec4(in_position.x, in_position.y, 0.0, 1.0);\n"
+		"}\n";
+
+	auto vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, 0);
 	glCompileShader(vertexShader);
-	glCompileShader(fragmentShader);
-	glAttachShader(program, vertexShader);
-	glAttachShader(program, fragmentShader);
-	glLinkProgram(program);
+	int result = 0;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+	compile_success = (bool)result;
+	if (!result)
+	{
+		int len = 0;
+		glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &len);
+		char* log = (char*)malloc(len);
+		glGetShaderInfoLog(vertexShader, len, 0, log);
+		printf("Shader Compilation Failed! : %s\n", log);
+		free(log);
+	}
 
-	bool showDemo = true;
-	bool want_exit = false;
+
+	const char* commonShaderSource =
+		"#version 450\n"
+		"out vec4 FinalColor;\n"
+		"in vec4 fragCoord;\n"
+		"uniform vec3 iResolution;\n"           // viewport resolution (in pixels)
+		"uniform float iTime;\n"           // shader playback time (in seconds)
+		"uniform float iTimeDelta;\n"          // render time (in seconds)
+		"uniform int iFrame;\n"         // shader playback frame
+		//"uniform float iChannelTime[4];\n"      // channel playback time (in seconds)
+		//"uniform vec3 iChannelResolution[4];\n" // channel resolution (in pixels)
+		"uniform vec4 iMouse;\n"                // mouse pixel coords. xy: current (if MLB down), zw: click
+		//"uniform vec4 iDate;\n"                 // (year, month, day, time in seconds)
+		//"uniform float iSampleRate;\n";           // sound sample rate (i.e., 44100)
+		"void mainImage( out vec4 fragColor, in vec2 fragCoord );\n"
+		"void main() {\n"
+		"mainImage(FinalColor, gl_FragCoord.xy);"
+		"};\n";
+
+	auto commonShaderSourceLen = strlen(commonShaderSource);
+
+	float iTime = 0, iTimeDelta = 0;
+	int iTimeLocation = 0, iResolutionLocation = 0, iTimeDeltaLocation = 0, iFrameLocation = 0, iChannelTimeLocation = 0, iChannelResolutionLocation = 0, iMouseLocation = 0, iDateLocation = 0, iSampleRateLocation = 0;
+	int iFrame = 0;
 
 	while (!glfwWindowShouldClose(window))
 	{
+		iFrame++;
+		iTime = (float)glfwGetTime();
 		glfwPollEvents();
 		glClear(GL_COLOR_BUFFER_BIT);
 
@@ -58,17 +122,10 @@ int main()
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
-		//ImGui::ShowDemoWindow(&showDemo);
-
-		ImGui::Begin("Status");
-		ImGui::Text("Want Exit? %s", want_exit ? "Yes" : "No");
+		ImGui::Begin("Code");
+		auto avail = ImGui::GetContentRegionAvail();
+		ImGui::InputTextMultiline("##GLSL", codeBuffer, codeBufferSize, avail);
 		ImGui::End();
-
-	
-		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		{
-			want_exit = true;
-		}
 
 		if (want_exit)
 		{
@@ -80,9 +137,6 @@ int main()
 			{
 				ImGui::Text("Are You Sure Want to Exit?");
 				ImGui::Separator();
-
-				//static int unused_i = 0;
-				//ImGui::Combo("Combo", &unused_i, "Delete\0Delete harder\0");
 		
 				if (ImGui::Button("OK", ImVec2(120, 0))) { ImGui::CloseCurrentPopup(); want_exit = false; glfwSetWindowShouldClose(window, 1); }
 				ImGui::SetItemDefaultFocus();
@@ -92,18 +146,93 @@ int main()
 			}
 		}
 
-		
 
-
-		if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+		if (want_save)
 		{
+			auto sourceLen = strlen(codeBuffer);
+			char* shaderSource = (char*)calloc(1, sourceLen + commonShaderSourceLen);
+			//strcpy(shaderSource, commonShaderSource);
+			strcat(shaderSource, commonShaderSource);
+			strcat(shaderSource, codeBuffer);
 
+			if(shader)
+				glDeleteShader(shader);
+			if(program)
+				glDeleteProgram(program);
+
+			program = glCreateProgram();
+			shader = glCreateShader(GL_FRAGMENT_SHADER);
+			glShaderSource(shader, 1, &shaderSource, 0);
+			glCompileShader(shader);
+			result = 0;
+			glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+			compile_success = (bool)result;
+			if (!result)
+			{
+				int len = 0;
+				glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+				char* log = (char*)malloc(len);
+				glGetShaderInfoLog(shader, len, 0, log);
+				printf("Shader Compilation Failed! : %s\n", log);
+				free(log);
+			}
+			glAttachShader(program, vertexShader);
+			glAttachShader(program, shader);
+			glLinkProgram(program);
+			glGetProgramiv(program, GL_LINK_STATUS, &result);
+			if (!result)
+			{
+				int len = 0;
+				glGetProgramiv(program, GL_INFO_LOG_LENGTH, &len);
+				char* log = (char*)malloc(len);
+				glGetProgramInfoLog(program, len, 0, log);
+				printf("Shader Link Failed! : %s\n", log);
+				free(log);
+			}
+			else
+			{
+				printf("Compile Success!\n");
+
+				iTimeLocation = glGetUniformLocation(program, "iTime");
+				iResolutionLocation = glGetUniformLocation(program, "iResolution");;
+				iTimeDeltaLocation = glGetUniformLocation(program, "iTimeDelta");;
+				iFrameLocation = glGetUniformLocation(program, "iFrame");;
+				//iChannelTimeLocation = glGetUniformLocation(program, "iChannelTime");
+				//iChannelResolutionLocation = glGetUniformLocation(program, "iChannelResolution");;
+				iMouseLocation = glGetUniformLocation(program, "iMouse");;
+				//iDateLocation = glGetUniformLocation(program, "iDate");;
+				//iSampleRateLocation = glGetUniformLocation(program, "iSampleRate");
+			}
+			want_save = false;
 		}
 
+		if (want_update)
+		{
+			want_update = false;
+		}
+
+		glUniform1f(iTimeLocation, iTime);
+		int x_size = 0, y_size = 0;
+		glfwGetFramebufferSize(window, &x_size, &y_size);
+		glUniform3f(iResolutionLocation, (float)x_size, (float)y_size, 0.0f);
+		glUniform1f(iTimeDeltaLocation, iTimeDelta);
+		glUniform1i(iFrameLocation, iFrame);
+		double mouse_x = 0, mouse_y = 0;
+		float left_click = 0, right_click = 0;
+		left_click = (float)glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+		right_click = (float)glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT);
+		glfwGetCursorPos(window, &mouse_x, &mouse_y);
+		glUniform4f(iMouseLocation, (float)mouse_x, (float)mouse_y, left_click, right_click);
+		glUseProgram(program);
+
 		glBegin(GL_QUADS);
+		//glTexCoord2i(1, 0);
 		glVertex2f(-1, 1);
+		//glTexCoord2i(0, 0);
 		glVertex2f(-1, -1);
+		//glTexCoord2i(0, 1);
 		glVertex2f( 1, -1);
+		//glTexCoord2i(1, 1);
 		glVertex2f(1, 1);
 		glEnd();
 
