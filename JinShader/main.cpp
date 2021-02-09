6331db7f0a54486871e4f4260382bc60f4332c95
Ahsan Ullah <geeksdotpk@gmail.com>
2021-02-09 23:04:50 +0500

    add main menu and console for logging shader errors

--- JinShader/main.cpp
+++ JinShader/main.cpp
@@ -1,5 +1,131 @@
 #include "JinShader.h"
 
+
+//this is borrowed from the imgui_demo.cpp
+struct ConsoleLog
+{
+	ImGuiTextBuffer     Buf;
+	ImGuiTextFilter     Filter;
+	ImVector<int>       LineOffsets; // Index to lines offset. We maintain this with AddLog() calls.
+	bool                AutoScroll;  // Keep scrolling if already at the bottom.
+
+	ConsoleLog()
+	{
+		AutoScroll = true;
+		Clear();
+	}
+
+	void Clear()
+	{
+		Buf.clear();
+		LineOffsets.clear();
+		LineOffsets.push_back(0);
+	}
+
+	void AddLog(const char* fmt, ...) IM_FMTARGS(2)
+	{
+		int old_size = Buf.size();
+		va_list args;
+		va_start(args, fmt);
+		Buf.appendfv(fmt, args);
+		va_end(args);
+		for (int new_size = Buf.size(); old_size < new_size; old_size++)
+			if (Buf[old_size] == '\n')
+				LineOffsets.push_back(old_size + 1);
+	}
+
+	void Draw(const char* title, bool* p_open = NULL)
+	{
+		if (!ImGui::Begin(title, p_open))
+		{
+			ImGui::End();
+			return;
+		}
+
+		// Options menu
+		if (ImGui::BeginPopup("Options"))
+		{
+			ImGui::Checkbox("Auto-scroll", &AutoScroll);
+			ImGui::EndPopup();
+		}
+
+		// Main window
+		if (ImGui::Button("Options"))
+			ImGui::OpenPopup("Options");
+		ImGui::SameLine();
+		bool clear = ImGui::Button("Clear");
+		ImGui::SameLine();
+		bool copy = ImGui::Button("Copy");
+		ImGui::SameLine();
+		Filter.Draw("Filter", -100.0f);
+
+		ImGui::Separator();
+		ImGui::BeginChild("scrolling", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar);
+
+		if (clear)
+			Clear();
+		if (copy)
+			ImGui::LogToClipboard();
+
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+		const char* buf = Buf.begin();
+		const char* buf_end = Buf.end();
+		if (Filter.IsActive())
+		{
+			// In this example we don't use the clipper when Filter is enabled.
+			// This is because we don't have a random access on the result on our filter.
+			// A real application processing logs with ten of thousands of entries may want to store the result of
+			// search/filter.. especially if the filtering function is not trivial (e.g. reg-exp).
+			for (int line_no = 0; line_no < LineOffsets.Size; line_no++)
+			{
+				const char* line_start = buf + LineOffsets[line_no];
+				const char* line_end = (line_no + 1 < LineOffsets.Size) ? (buf + LineOffsets[line_no + 1] - 1) : buf_end;
+				if (Filter.PassFilter(line_start, line_end))
+					ImGui::TextUnformatted(line_start, line_end);
+			}
+		}
+		else
+		{
+			// The simplest and easy way to display the entire buffer:
+			//   ImGui::TextUnformatted(buf_begin, buf_end);
+			// And it'll just work. TextUnformatted() has specialization for large blob of text and will fast-forward
+			// to skip non-visible lines. Here we instead demonstrate using the clipper to only process lines that are
+			// within the visible area.
+			// If you have tens of thousands of items and their processing cost is non-negligible, coarse clipping them
+			// on your side is recommended. Using ImGuiListClipper requires
+			// - A) random access into your data
+			// - B) items all being the  same height,
+			// both of which we can handle since we an array pointing to the beginning of each line of text.
+			// When using the filter (in the block of code above) we don't have random access into the data to display
+			// anymore, which is why we don't use the clipper. Storing or skimming through the search result would make
+			// it possible (and would be recommended if you want to search through tens of thousands of entries).
+			ImGuiListClipper clipper;
+			clipper.Begin(LineOffsets.Size);
+			while (clipper.Step())
+			{
+				for (int line_no = clipper.DisplayStart; line_no < clipper.DisplayEnd; line_no++)
+				{
+					const char* line_start = buf + LineOffsets[line_no];
+					const char* line_end = (line_no + 1 < LineOffsets.Size) ? (buf + LineOffsets[line_no + 1] - 1) : buf_end;
+					ImGui::TextUnformatted(line_start, line_end);
+				}
+			}
+			clipper.End();
+		}
+		ImGui::PopStyleVar();
+
+		if (AutoScroll && ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
+			ImGui::SetScrollHereY(1.0f);
+
+		ImGui::EndChild();
+		ImGui::End();
+	}
+};
+
+static ConsoleLog consoleLogger;
+
+
+
 int main()
 {
 
@@ -19,12 +145,11 @@ int main()
 	glGenFramebuffers(1, &fbo);
 
 	const char* vertexShaderSource = 
-		"#version 460\n"
-		"layout(location = 0) in vec2 in_position;\n"
-		//"out vec2 fragCoord;\n"
+		"#version 450\n"
+		"layout(location = 0) in vec4 in_position;\n"
 		"void main()\n"
 		"{\n"
-			"gl_Position = vec4(in_position.x, in_position.y, 0.0, 1.0);\n"
+			"gl_Position = in_position;\n"
 		"}\n";
 
 	auto vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -99,10 +224,15 @@ int main()
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+	bool showAboutImGui = false;
+	bool showAboutJinShader = false;
+	bool showCode = true;
+	bool showLog = true;
+
 	while (state->window_is_open)
 	{
 		JinShaderUpdate(state);
-		//if (state->has_focus)
+		if (state->has_focus)
 		{
 			iFrame++;
 			iTime = (float)glfwGetTime();
@@ -112,14 +242,71 @@ int main()
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
 
-			ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0, 0 });
 			ImGui::DockSpaceOverViewport();
-			ImGui::Begin("Code", 0);
-			auto avail = ImGui::GetContentRegionAvail();
-			ImGui::InputTextMultiline("##GLSL", codeBuffer, codeBufferSize, avail);
-			ImGui::End();
+			if (ImGui::BeginMainMenuBar())
+			{
+				if (ImGui::BeginMenu("File"))
+				{
+					if (ImGui::MenuItem("Exit"))
+						state->want_exit = true;
+					ImGui::EndMenu();
+				}
+
+				if (ImGui::BeginMenu("View"))
+				{
+					ImGui::MenuItem("Show Code", 0, &showCode);
+					ImGui::MenuItem("Show Log", 0, &showLog); 
+
+					ImGui::EndMenu();
+				}
+
+				if (ImGui::BeginMenu("About"))
+				{
+					if (ImGui::MenuItem("JinShader"))
+					{
+						showAboutJinShader = true;
+					}
+					if (ImGui::MenuItem("Dear ImGui"))
+					{
+						showAboutImGui = true;
+					}
+					ImGui::EndMenu();
+				}
+				ImGui::EndMainMenuBar();
+			}
+
+			//ImGui::ShowDemoWindow();
+
+			if(showAboutImGui)
+				ImGui::ShowAboutWindow(&showAboutImGui);
+			if (showAboutJinShader)
+			{
+				if (ImGui::Begin("About JinShader", &showAboutJinShader, ImGuiWindowFlags_AlwaysAutoResize))
+				{
+					ImGui::Text("Version 0.4 pre-release");
+					ImGui::Separator();
+					ImGui::Text("Author: Ahsan Ullah Sarbaz");
+					ImGui::End();
+				}
+
+			}
+
+			ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0, 0 });
+			
+			if (showLog)
+			{
+				consoleLogger.Draw("Log", &showLog);
+			}
+			if (showCode)
+			{
+				ImGui::Begin("Code", &showCode);
+				auto avail = ImGui::GetContentRegionAvail();
+				ImGui::InputTextMultiline("##GLSL", codeBuffer, codeBufferSize, avail);
+				ImGui::End();
+			}
+
 			ImGui::Begin("View", 0);
-			avail = ImGui::GetContentRegionAvail();
+			auto avail = ImGui::GetContentRegionAvail();
 			ImGui::Image(reinterpret_cast<void*>(colorAttachMent), avail, { 0,1 }, {1,0});
 			framebufferSizeX = avail.x;
 			framebufferSizeY = avail.y;
@@ -140,7 +327,6 @@ int main()
 			if (state->want_exit)
 			{
 				ImGui::OpenPopup("Exit?");
-				// Always center this window when appearing
 				ImVec2 center(ImGui::GetIO().DisplaySize.x * 0.5f, ImGui::GetIO().DisplaySize.y * 0.5f);
 				ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
 				if (ImGui::BeginPopupModal("Exit?", NULL, ImGuiWindowFlags_AlwaysAutoResize))
@@ -194,6 +380,7 @@ int main()
 					glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
 					char* log = (char*)malloc(len);
 					glGetShaderInfoLog(shader, len, 0, log);
+					consoleLogger.AddLog("Shader Compilation Failed %s", log);
 					printf("Shader Compilation Failed! : %s\n", log);
 					free(log);
 				}
@@ -207,6 +394,7 @@ int main()
 					glGetProgramiv(program, GL_INFO_LOG_LENGTH, &len);
 					char* log = (char*)malloc(len);
 					glGetProgramInfoLog(program, len, 0, log);
+					consoleLogger.AddLog("Shader Link Failed %s", log);
 					printf("Shader Link Failed! : %s\n", log);
 					free(log);
 				}
@@ -261,7 +449,7 @@ int main()
 			glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
 #ifdef _DEBUG
-			ImGui::Begin("State");
+			ImGui::Begin("State", 0, ImGuiWindowFlags_AlwaysAutoResize);
 			ImGui::Text("Region Avail %f, %f", avail.x, avail.y);
 			ImGui::Text("Texture Size %d, %d", state->fb_width, state->fb_height);
 
@@ -275,8 +463,6 @@ int main()
 
 		}
 	}
-
-
 
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
